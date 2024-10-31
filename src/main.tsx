cbe92819933dcec16d9685a6cd75ab9f7a088983
thecodermehedi <iammrmehedi@outlook.com>
2024-10-31 12:49:57 +0600

    add form

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import UserInfoWithUseReducer from './components/UserInfoWithUseReducer'
 import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <UserInfoWithUseReducer />
   </StrictMode>,
 )
