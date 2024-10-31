c91cb4e871d8b64e94e262a7d6094437e676618f
Mehedi Hasan <mehedi@etlimited.net>
2024-10-31 13:30:34 +0600

    update 2 files and delete 3 files

--- src/components/UserInfoWithUseReducer.tsx
+++ src/components/UserInfoWithUseReducer.tsx
@@ -1,68 +1,89 @@
+import { useReducer } from "react";
+
+const initialState = {
+ name: "",
+ age: "",
+ hobbies: []
+}
+
+const reducer = (currentState, action) => {
+ switch (action.type) {
+  case "add_name":
+   return { ...currentState, name: "programming-hero" }
+   break;
+
+  default:
+   break;
+ }
+}
+
 const UserInfoWithUseReducer = () => {
-    return (
-        <div className="min-h-screen bg-gray-100 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-          <div className="max-w-md w-full space-y-8">
-            <div>
-              <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-                User Information
-              </h2>
-            </div>
-            <form className="mt-8 space-y-6">
-              <div className="rounded-md shadow-sm -space-y-px">
-                <div>
-                  <label htmlFor="name" className="sr-only">
-                    Name
-                  </label>
-                  <input
-                    id="name"
-                    name="name"
-                    type="text"
-                    required
-                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                    placeholder="Name"
-                  />
-                </div>
-                <div>
-                  <label htmlFor="age" className="sr-only">
-                    Age
-                  </label>
-                  <input
-                    id="age"
-                    name="age"
-                    type="number"
-                    required
-                    min="0"
-                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                    placeholder="Age"
-                  />
-                </div>
-                <div>
-                  <label htmlFor="hobbies" className="sr-only">
-                    Hobbies
-                  </label>
-                  <input
-                    id="hobbies"
-                    name="hobbies"
-                    type="text"
-                    required
-                    className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                    placeholder="Hobbies (comma-separated)"
-                  />
-                </div>
-              </div>
-    
-              <div>
-                <button
-                  type="submit"
-                  className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                  Save Information
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      )
+
+ const [state, dispatch] = useReducer(reducer, initialState);
+ console.log(state);
+
+
+ return (
+  <div className="min-h-screen bg-gray-100 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+   <div className="max-w-md w-full space-y-8">
+    <form className="mt-8 space-y-6">
+     <div className="rounded-md shadow-sm -space-y-px">
+      <div>
+       <label htmlFor="name" className="sr-only">
+        Name
+       </label>
+       <input
+        className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+        type="text"
+        name="name"
+        id="name"
+        placeholder="Name"
+        onChange={() => dispatch({ type: "add_name" })}
+       />
+      </div>
+      <div>
+       <label htmlFor="age" className="sr-only">
+        Age
+       </label>
+       <input
+        className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+        type="number"
+        name="age"
+        id="age"
+        placeholder="Age"
+        min="0"
+       />
+      </div>
+      <div>
+       <label htmlFor="hobbies" className="sr-only">
+        Hobbies
+       </label>
+       <input
+        className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+        type="text"
+        name="hobbies"
+        id="hobbies"
+        placeholder="Hobbies (comma-separated)"
+       />
+      </div>
+     </div>
+
+     <code>
+      { }
+     </code>
+
+     <div>
+      <button
+       type="submit"
+       className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      >
+       Save Information
+      </button>
+     </div>
+    </form>
+   </div>
+  </div>
+ )
 };
 
 export default UserInfoWithUseReducer;
